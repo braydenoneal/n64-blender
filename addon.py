dfaa0e27f782959281be3531b99b1710ec606ac4
Brayden O'Neal <braydenoneal@protonmail.com>
2024-12-22 10:16:02 -0600

    Add ambient color and light color toggles

--- addon.py
+++ addon.py
@@ -108,32 +108,6 @@ def create_4b_material(obj):
 def reset_material(context, material):
     nodes = material.node_tree.nodes
 
-    shader_settings = nodes["Shader"]
-    shader_inputs = shader_settings.inputs
-
-    shader_inputs["Enable Solid Color"].default_value = context.scene.props.enable_solid_color
-
-    shader_inputs["Enable Transparency"].default_value = (context.scene.props.transparency_mode != "opaque" and
-                                                          not context.scene.props.enable_solid_color)
-
-    transparency_mode_to_blend_method = {
-        "opaque": "OPAQUE",
-        "cutout": "CLIP",
-        "transparent": "BLEND"
-    }
-
-    material.blend_method = transparency_mode_to_blend_method[context.scene.props.transparency_mode]
-
-    material.use_backface_culling = context.scene.props.enable_backface_culling
-
-    shader_inputs["Enable Fog"].default_value = context.scene.props.enable_fog
-    shader_inputs["Enable Vertex Colors"].default_value = context.scene.props.enable_vertex_colors
-
-    shader_inputs["Enable Overlay Color"].default_value = context.scene.props.enable_overlay_color
-
-    shader_inputs["Solid Color"].default_value = context.scene.props.solid_color
-    shader_inputs["Overlay Color"].default_value = context.scene.props.overlay_color
-
     for i in ["0, 0", "0, 1", "1, 0", "1, 1"]:
         texture = nodes[f"Texture {i}"]
         texture.image = context.scene.props.texture
@@ -157,6 +131,38 @@ def reset_material(context, material):
     filter_inputs["Repeat Y"].default_value = y_bounds == "repeat"
     filter_inputs["Mirror Y"].default_value = y_bounds == "mirror"
 
+    shader_settings = nodes["Shader"]
+    shader_inputs = shader_settings.inputs
+
+    shader_inputs["Enable Transparency"].default_value = (context.scene.props.transparency_mode != "opaque" and
+                                                          not context.scene.props.enable_solid_color)
+
+    transparency_mode_to_blend_method = {
+        "opaque": "OPAQUE",
+        "cutout": "CLIP",
+        "transparent": "BLEND"
+    }
+
+    material.blend_method = transparency_mode_to_blend_method[context.scene.props.transparency_mode]
+
+    material.use_backface_culling = context.scene.props.enable_backface_culling
+
+    shader_inputs["Solid Color"].default_value = context.scene.props.solid_color
+
+    shader_inputs["Enable Solid Color"].default_value = context.scene.props.enable_solid_color
+
+    shader_inputs["Enable Vertex Colors"].default_value = context.scene.props.enable_vertex_colors
+
+    shader_inputs["Overlay Color"].default_value = context.scene.props.overlay_color
+
+    shader_inputs["Enable Overlay Color"].default_value = context.scene.props.enable_overlay_color
+
+    shader_inputs["Enable Ambient Color"].default_value = context.scene.props.enable_ambient_color
+
+    shader_inputs["Enable Light Color"].default_value = context.scene.props.enable_light_color
+
+    shader_inputs["Enable Fog"].default_value = context.scene.props.enable_fog
+
 
 class Create4BMaterial(bpy.types.Operator):
     bl_idname = "object.create_4b_mat"
@@ -179,17 +185,6 @@ def update(_self, context):
 
 
 class Props(bpy.types.PropertyGroup):
-    enable_solid_color: bpy.props.BoolProperty(name="Enable Solid Color", default=False, update=update)
-    solid_color: bpy.props.FloatVectorProperty(
-        name="Solid Color",
-        subtype="COLOR",
-        size=4,
-        min=0,
-        max=1,
-        default=(1, 1, 1, 1),
-        update=update
-    )
-
     texture: bpy.props.PointerProperty(name="Texture", type=bpy.types.Image, update=update)
 
     bound_options = [
@@ -212,7 +207,17 @@ class Props(bpy.types.PropertyGroup):
                                               update=update)
     enable_backface_culling: bpy.props.BoolProperty(name="Enable Backface Culling", default=True, update=update)
 
-    enable_fog: bpy.props.BoolProperty(name="Enable Fog", default=True, update=update)
+    enable_solid_color: bpy.props.BoolProperty(name="Enable Solid Color", default=False, update=update)
+    solid_color: bpy.props.FloatVectorProperty(
+        name="Solid Color",
+        subtype="COLOR",
+        size=4,
+        min=0,
+        max=1,
+        default=(1, 1, 1, 1),
+        update=update
+    )
+
     enable_vertex_colors: bpy.props.BoolProperty(name="Enable Vertex Colors", default=True, update=update)
 
     enable_overlay_color: bpy.props.BoolProperty(name="Enable Overlay Color", default=False, update=update)
@@ -225,6 +230,12 @@ class Props(bpy.types.PropertyGroup):
         default=(1, 1, 1, 1),
         update=update
     )
+
+    enable_ambient_color: bpy.props.BoolProperty(name="Enable Ambient Color", default=True, update=update)
+
+    enable_light_color: bpy.props.BoolProperty(name="Enable Light Color", default=True, update=update)
+
+    enable_fog: bpy.props.BoolProperty(name="Enable Fog", default=True, update=update)
 
 
 class Panel(bpy.types.Panel):
@@ -244,10 +255,6 @@ class Panel(bpy.types.Panel):
             layout.label(text="This is not a 4B material.")
             return
 
-        layout.prop(props, "enable_solid_color")
-        if props.enable_solid_color:
-            layout.prop(props, "solid_color")
-
         if not props.enable_solid_color:
             layout.template_ID(props, "texture", open="image.open", new="image.new")
 
@@ -262,12 +269,21 @@ class Panel(bpy.types.Panel):
 
         layout.prop(props, "enable_backface_culling")
 
-        layout.prop(props, "enable_fog")
+        layout.prop(props, "enable_solid_color")
+        if props.enable_solid_color:
+            layout.prop(props, "solid_color")
+
         layout.prop(props, "enable_vertex_colors")
 
         layout.prop(props, "enable_overlay_color")
         if props.enable_overlay_color:
             layout.prop(props, "overlay_color")
+
+        layout.prop(props, "enable_ambient_color")
+
+        layout.prop(props, "enable_light_color")
+
+        layout.prop(props, "enable_fog")
 
 
 def register():
